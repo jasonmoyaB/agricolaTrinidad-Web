12af477b14bbe605299b12ba4a1a4958a5b7ddef
jason moya brenes <jason.moyabre.es@gmail.com>
2025-05-16 09:44:39 -0600

    cambio el carrusel

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ function toggleMenu() {
     nav.classList.toggle('active');
   }
  
+
+/*carrusel,galeria*/ 
+let currentIndex = 0;
+
+  function moveSlide(direction) {
+    const carousel = document.querySelector('.carousel');
+    const totalItems = document.querySelectorAll('.carousel-item').length;
+
+    if (!carousel) {
+      console.error("No se encontró el contenedor del carrusel.");
+      return;
+    }
+
+    currentIndex += direction;
+    if (currentIndex < 0) currentIndex = totalItems - 1;
+    if (currentIndex >= totalItems) currentIndex = 0;
+
+    const translateX = -currentIndex * 100;
+    carousel.style.transform = `translateX(${translateX}%)`;
+  }
